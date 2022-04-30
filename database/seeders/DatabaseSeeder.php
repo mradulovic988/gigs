10c42ea3b033b392623d5040dd32b70c3a82e3bc
markopopart <upss070288@gmail.com>
2022-05-01 00:01:16 +0200

    Added fille upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Listing::factory(6)->create();
+        Listing::factory(10)->create();
 
 //        Listing::create([
 //            'title' => 'Laravel Senior Developer',
