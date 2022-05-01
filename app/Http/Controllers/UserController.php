778d2563ee9a5cf15a721c45280334c3e112b359
markopopart <upss070288@gmail.com>
2022-05-01 17:32:52 +0200

    Create users login/logout/register features

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Unique;
+
+class UserController extends Controller
+{
+    // Show register/create form
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    // Create New Users
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6']
+        ]);
+
+        // Hash password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        $users = User::create($formFields);
+
+        // Login
+        auth()->login($users);
+        return redirect('/')->with('message', 'User created and logged in.');
+    }
+
+    // Logout
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out.');
+    }
+
+    // Show login form
+    public function login()
+    {
+        return view('users.login');
+    }
+
+    // Authenticate user
+    public function authenticate(Request $request)
+    {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You have successfully logged in.');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid Credentials'
+        ])->onlyInput('email');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -36,3 +37,18 @@ Route::delete('listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show register/create form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+// Logout users
+Route::post('/logout', [UserController::class, 'logout']);
+
+// Show login form
+Route::get('/login', [UserController::class, 'login']);
+
+// Log in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
